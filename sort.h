cb0ad4a31cccf622a3e3bac059e6aadb21232fc3
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-10-20 19:33:58 -0700

    task 7

--- sort.h
+++ sort.h
@@ -29,7 +29,7 @@ void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void merge(int *array, int *left, int left_size, int *right,
-                int right_size, size_t size);
+		int right_size, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
