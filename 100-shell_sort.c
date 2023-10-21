977b5bd3a02b5df5fb2db35e85fe8a85a6ddf7bb
esraaashraf023 <esraaashraf780567@gmail.com>
2023-10-20 21:22:25 -0700

    add file shell

---
+++ 100-shell_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Shell sort algorithm
+ * @array: Array.....
+ * @size: nums of array..
+ *
+ * Return: Null
+ */
+void shell_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, j, gap;
+
+	if (array == NULL || size == 0)
+		return;
+	{
+		gap = 1;
+		while (gap < size)
+			gap = gap * 3 + 1;
+		gap = (gap - 1) / 3;
+		for (; gap > 0; gap = (gap - 1) / 3)
+		{
+			for (i = gap; i < size; i++)
+			{
+				temp = array[i];
+				for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+					array[j] = array[j - gap];
+				array[j] = temp;
+			}
+			print_array(array, size);
+		}
+	}
+}
