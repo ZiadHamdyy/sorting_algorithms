53177c22e656ae0f277e70eb338e23d78b21ddd5
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-10-20 19:32:36 -0700

    task 7

---
+++ 103-merge_sort.c
@@ -0,0 +1,82 @@
+#include "sort.h"
+/**
+ * merge - .........
+ * @array:unsorted array.
+ * @left:the s first array.
+ * @left_size:the size of left array.
+ * @right:the second.
+ * @right_size: the size of right array.
+ * @size:size of the array.
+ * Return:void.
+ */
+void merge(int *array, int *left, int left_size, int *right,
+		int right_size, size_t size)
+{
+	int i = 0, j = 0, k = 0;
+
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(left, left_size);
+	printf("[right]: ");
+	print_array(right, right_size);
+	while (i < left_size && j < right_size)
+	{
+		if (left[i] <= right[j])
+		{
+			array[k] = left[i];
+			i++;
+		}
+		else
+		{
+			array[k] = right[j];
+			j++;
+		}
+		k++;
+	}
+	while (i < left_size)
+	{
+		array[k] = left[i];
+		i++;
+		k++;
+	}
+
+	while (j < right_size)
+	{
+		array[k] = right[j];
+		j++;
+		k++;
+	}
+	printf("[Done]: ");
+	print_array(array, size);
+}
+/**
+ * merge_sort - ................
+ * @array: unsorted array.
+ * @size:size of the array.
+ * Return:void.
+ */
+void merge_sort(int *array, size_t size)
+{
+	int middle = size / 2;
+	int *left = array;
+	int *right = array + middle;
+	int *temp;
+	size_t i;
+
+	if (size <= 1)
+		return;
+
+	merge_sort(left, middle);
+	merge_sort(right, size - middle);
+	temp = (int *)malloc(size * sizeof(int));
+	if (temp == NULL)
+		exit(1);
+
+	merge(temp, left, middle, right, size - middle, size);
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = temp[i];
+	}
+	free(temp);
+}
