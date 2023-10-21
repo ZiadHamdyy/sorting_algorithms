15d8d87483e58c5469b130d18465df94ade7771f
esraaashraf023 <esraaashraf780567@gmail.com>
2023-10-20 22:50:45 -0700

    add file sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -5,7 +5,6 @@
  * @array: Array.....
  * @size: nums of array..
  *
- * Return: Null
  */
 void shell_sort(int *array, size_t size)
 {
@@ -14,7 +13,7 @@ void shell_sort(int *array, size_t size)
 
 	if (array == NULL || size == 0)
 		return;
-
+	{
 		gap = 1;
 		while (gap < size)
 			gap = gap * 3 + 1;
@@ -30,6 +29,7 @@ void shell_sort(int *array, size_t size)
 			}
 			print_array(array, size);
 		}
+	}
 }
 
 /**
