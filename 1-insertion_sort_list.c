c3ede4a53cf3a4a435d69e2713c8354576ff7501
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-10-23 05:20:29 -0700

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,25 +1,5 @@
 #include "sort.h"
 /**
- * swap_nodes - ..............
- * @list:unsorted list.
- * @node1:first pointer.
- * @node2:second pointer.
- * Return:void.
- */
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
-{
-	 node2->prev = node1->prev;
-	 node1->next = node2->next;
-	if (node2->next)
-		node2->next->prev = node1;
-	node1->prev = node2;
-	node2->next = node1;
-	if (!node2->prev)
-		*list = node2;
-	else
-		node2->prev->next = node2;
-}
-/**
  * insertion_sort_list - .........
  * @list:unsorted list.
  * Return:void.
@@ -39,6 +19,26 @@ void insertion_sort_list(listint_t **list)
 			swap_nodes(list, ptr->prev, current);
 			print_list(*list);
 		}
-	current = current->next;
+		current = current->next;
 	}
+}
+/**
+ * swap_nodes - ..............
+ * @list:unsorted list.
+ * @node1:first pointer.
+ * @node2:second pointer.
+ * Return:void.
+ */
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+{
+	 node2->prev = node1->prev;
+	 node1->next = node2->next;
+	if (node2->next)
+		node2->next->prev = node1;
+	node1->prev = node2;
+	node2->next = node1;
+	if (!node2->prev)
+		*list = node2;
+	else
+		node2->prev->next = node2;
 }
